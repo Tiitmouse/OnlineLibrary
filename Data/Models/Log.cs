43bae820cfb200ad4367061d5178395196ad2deb
Tiitmouse <lkovacic380@gmail.com>
2025-01-04 16:00:07 +0100

    Logs added

---
+++ Data/Models/Log.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data.Models;
+
+public partial class Log
+{
+    public int IdLog { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public string Message { get; set; } = null!;
+
+    public int Importance { get; set; }
+}
