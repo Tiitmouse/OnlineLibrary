db6aeee8f8fc367d07ca1d1bd01d7a6bb1ba4bb3
Tiitmouse <lkovacic380@gmail.com>
2025-01-19 18:57:13 +0100

    user full name update

--- WebApp/Controllers/UserController.cs
+++ WebApp/Controllers/UserController.cs
@@ -22,12 +22,12 @@ public class UserController : Controller
         _userServices = userServices;
         _mapper = mapper;
     }
-    
+
     public IActionResult Login(string returnUrl)
     {
         return View();
     }
-    
+
     [HttpPost]
     public async Task<IActionResult> Login([FromForm] UserLoginDto dto)
     {
@@ -48,12 +48,12 @@ public class UserController : Controller
             return BadRequest(ex.Message);
         }
     }
-    
+
     public IActionResult Register()
     {
         return View();
     }
-    
+
     [HttpPost]
     public async Task<IActionResult> Register(User user, string password)
     {
@@ -73,50 +73,26 @@ public class UserController : Controller
         await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
         return RedirectToAction("Index", "Home");
     }
-    
+
     public async Task<IActionResult> Details(string username)
     {
         var user = await _userServices.GetUser(username);
         var userDetails = _mapper.Map<UserDetailsViewModel>(user);
         return View(userDetails);
     }
-    
+
     [HttpPost]
     public async Task<IActionResult> EditUserFullName(UserDetailsViewModel model)
     {
-        if (ModelState.IsValid)
-        {
-            var username = model.Username; // Assuming the username is stored in the identity
-            await _userServices.UpdateUserFullName(model.FullName, username);
-
-            return Json(new { success = true });
-        }
-
-        // Return validation errors
-        var errors = ModelState.ToDictionary(
-            kvp => kvp.Key,
-            kvp => kvp.Value.Errors.Select(e => e.ErrorMessage).ToArray()
-        );
-
-        return Json(new { success = false, errors });
+        var username = model.Username; 
+        await _userServices.UpdateUserFullName(model.FullName, username);
+        return Json(new { success = true });
     }
-    
+
     [HttpPost]
-    public async Task<IActionResult> EditUserPassword(string oldp, string newp, string username) 
+    public async Task<IActionResult> EditUserPassword(string oldp, string newp, string username)
     {
-        if (ModelState.IsValid)
-        { 
-            await _userServices.UpdateUserPassword(oldp, newp, username);
-
-            return Json(new { success = true });
-        }
-
-        // Return validation errors
-        var errors = ModelState.ToDictionary(
-            kvp => kvp.Key,
-            kvp => kvp.Value.Errors.Select(e => e.ErrorMessage).ToArray()
-        );
-
-        return Json(new { success = false, errors });
+        await _userServices.UpdateUserPassword(oldp, newp, username);
+        return Json(new { success = true });
     }
 }
